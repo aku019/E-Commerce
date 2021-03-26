11b875d72e599370c0c2d83445151984838b402b
Asish kumar Upadhyay <asishupadhyay24@gmail.com>
2021-03-27 01:02:33 +0530

    first

--- server/server.js
+++ server/server.js
@@ -61,9 +61,9 @@ const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname, '/frontend/build')))
+    app.use(express.static(path.join(__dirname, '/src/build')))
     
-    app.get('*', (req, res)=> res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))  
+    app.get('*', (req, res)=> res.sendFile(path.resolve(__dirname, 'src', 'build', 'index.html')))  
 } else {
     app.get('/', (req,res)=>{
         res.send('API is running...')
